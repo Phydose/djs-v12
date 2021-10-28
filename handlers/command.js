39306e11cb938df1f9c3eeb8fe40d63793770fba
Phydose <56157625+Phydose@users.noreply.github.com>
2021-10-28 06:11:06 +0100

    Update command.js

--- handlers/command.js
+++ handlers/command.js
@@ -11,13 +11,14 @@ module.exports = (client) => {
                 client.commands.set(pull.name, pull);
                 table.addRow(file, '✅')
             } else {
-                table.addRow(file, '❌ -> Missing a help.name, or help.name is not a string.')
+                table.addRow(file, '❌')
                 continue;
             }
             if (pull.aliases && Array.isArray(pull.aliases)) pull.aliases.forEach(alias => client.aliases.set(alias, pull.name))
         }
     });
     console.log(table.toString());
+    console.log('[HANDLER]: Commands loaded !')
 
 
 
@@ -36,4 +37,5 @@ module.exports = (client) => {
             }
         }
     });
+    console.log('[HANDLER]: Events loaded !')
 }
