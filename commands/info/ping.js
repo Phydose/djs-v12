4ef7029663f350b2ffe6042b86ddd7e9a52938a6
Phydose <56157625+Phydose@users.noreply.github.com>
2021-10-28 06:13:55 +0100

    Update ping.js

--- commands/info/ping.js
+++ commands/info/ping.js
@@ -3,13 +3,6 @@ module.exports = {
     name : 'ping',
     category : 'info',
     description : 'Returns latency and API ping',
-
-    /**
-     * @param {Client} client
-     * @param {Message} message
-     * @param {String[]} args
-     */
-
     run : async(client, message, args) => {
         const msg = await message.channel.send(`🏓 Pinging...`)
         const embed = new MessageEmbed()
