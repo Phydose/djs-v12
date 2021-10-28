7aecdb4c6742c1a90a4ff15cb4e6fbb649983b37
Phydose <56157625+Phydose@users.noreply.github.com>
2021-10-28 06:08:04 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 const { Collection, Client } = require('discord.js')
 const fs = require('fs')
+require('dotenv').config
 
 const client = new Client({
     disableEveryone: true
 })
 
-const config = require('./config.json')
-const token = config.token
+client.config = require('./config.json')
+const token = process.env;
 
 module.exports = client
+
 
 client.commands = new Collection();
 client.aliases = new Collection();
 client.categories = fs.readdirSync("./commands/");
+
+
 ["command"].forEach(handler => {
     require(`./handlers/${handler}`)(client);
 }); 
